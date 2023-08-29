3240181307c1dd21c4c268e88aa8910bbb9db166
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-08-29 23:16:37 +0100

    Add place_ship

--- run.py
+++ run.py
@@ -2,6 +2,15 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+"""
+~ for empty (water)
+S for a ship
+H for a hit
+M for a miss
+"""
+
+import random
+
 def initialize_grid(size):
     """
     Create a grid of size 'size x size' filled with '~'
@@ -11,14 +20,26 @@ def initialize_grid(size):
     return grid
 
 def display_grid(grid):
-    # Display the grid to the console
+    """Display the grid to the console"""
     for row in grid:
         print(' '.join(row))
 
 
-# Initialize a 5x5 grid
-grid_size = 5
-grid = initialize_grid(grid_size)
 
-# Display the initial grid
-display_grid(grid)
+
+
+
+def place_ship(grid, row, col, orientation, length):
+    """
+    Place a ship of length 'length' in the grid at row 'row' and column 'col'
+    """
+    if orientation == 'H':
+        for i in range(length):
+            grid[row][col + i] = 'S'
+    elif orientation == 'V':
+        for i in range(length):
+            grid[row + i][col] = 'S'
+
+# Initialize a 5x5 grid for player and computer
+player_grid = initialize_grid(5)
+computer_grid = initialize_grid(5)
