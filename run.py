36dacefb1dc7b1928580b32cacae249456d9bb11
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-08-29 23:05:38 +0100

    Add initialise_grid

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def initialize_grid(size):
+    """
+    Create a grid of size 'size x size' filled with '~'
+    ~ represents empty water
+    """
+    grid = [['~' for _ in range(size)] for _ in range(size)]
+    return grid
+
+
+
+# Initialize a 5x5 grid
+grid_size = 5
+grid = initialize_grid(grid_size)
+
