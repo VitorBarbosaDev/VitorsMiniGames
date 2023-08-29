1354c0ede92f032b807d6b19f07c3af248c8219a
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-08-29 23:20:05 +0100

    Add place_player_ships

--- run.py
+++ run.py
@@ -40,6 +40,23 @@ def place_ship(grid, row, col, orientation, length):
         for i in range(length):
             grid[row + i][col] = 'S'
 
+def place_player_ships(grid, ship_length):
+    display_grid(player_grid)
+    row = int(input("Enter the row number to place your ship: "))
+    col = int(input("Enter the column number to place your ship: "))
+    orientation = input("Enter the orientation (H for horizontal, V for vertical): ")
+    place_ship(grid, row, col, orientation, ship_length)
+
 # Initialize a 5x5 grid for player and computer
 player_grid = initialize_grid(5)
 computer_grid = initialize_grid(5)
+
+
+
+# Place a 3-cell long ship for player
+place_player_ships(player_grid, 3)
+
+
+# Display the grids
+print("Player Grid:")
+display_grid(player_grid)
