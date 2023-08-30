8d4b5dd58510c470fbf3c6d91f714b92ea98e82b
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-08-30 21:32:47 +0100

    Add validation check for placing ships

--- run.py
+++ run.py
@@ -20,9 +20,19 @@ def initialize_grid(size):
     grid = [['~' for _ in range(size)] for _ in range(size)]
     return grid
 
+
 def get_grid_size():
     """Set size of grid"""
-    return int(input("Enter the grid size (e.g., 5 for a 5x5 grid): "))
+    while True:
+        try:
+            size = int(input("Enter the grid size (e.g., 5 for a 5x5 grid): "))
+            if size >= 5:  # Assuming 5 is the minimum acceptable size
+                return size
+            else:
+                print("Please enter a grid size of at least 5.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
 
 def display_grid(grid):
     """Display the grid to the console"""
@@ -47,7 +57,7 @@ def place_ship(grid, row, col, orientation, length):
             grid[row + i][col] = 'S'
 
 
-def place_multiple_ships(grid, ship_lengths,placer):
+def place_multiple_ships(grid, ship_lengths, placer):
     """Place multiple ships in the grid"""
     for length in ship_lengths:
         if placer == "player":
@@ -56,26 +66,67 @@ def place_multiple_ships(grid, ship_lengths,placer):
             place_computer_ships(grid, length)
 
 
+def is_within_grid(row, col, orientation, length, grid_size):
+    """Checks if placement is within grid"""
+    if orientation == 'H':
+        return col + length <= grid_size
+    elif orientation == 'V':
+        return row + length <= grid_size
+    return False
+
+def is_empty(grid, row, col, orientation, length):
+    """Checks if placement is empty"""
+    if orientation == 'H':
+        return all(grid[row][col + i] == '~' for i in range(length))
+    elif orientation == 'V':
+        return all(grid[row + i][col] == '~' for i in range(length))
+    return False
+
+def is_valid_placement(grid, row, col, orientation, length):
+    """Checks if placement is valid"""
+    grid_size = len(grid)
+    return is_within_grid(row, col, orientation, length, grid_size) and is_empty(grid, row, col, orientation, length)
+
+
+
 def place_player_ships(grid, ship_length):
     """Player can enter a place and direction they want their ship to be placed in"""
-    display_grid(grid)
-    print("Where do you want to place your ship of length " + str(ship_length) + "?")
-    row = int(input("Enter the row number to place your ship: \n"))
-    col = int(input("Enter the column number to place your ship: \n"))
-    orientation = input("Enter the orientation (H for horizontal, V for vertical): \n").upper()
-    place_ship(grid, row - 1, col - 1, orientation, ship_length)
+    while True:
+        display_grid(grid)
+        try:
+            row = int(input(f"Enter the row number to place your {ship_length}-cell ship: "))
+            col = int(input("Enter the column number: "))
+            orientation = input("Enter the orientation (H for horizontal, V for vertical): ").upper()
+
+            if orientation not in ['H', 'V']:
+                print("Invalid orientation. Please enter H or V.")
+                continue
+
+            if is_valid_placement(grid, row - 1, col - 1, orientation, ship_length):
+                place_ship(grid, row - 1, col - 1, orientation, ship_length)
+                break
+            else:
+                print("Invalid placement. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 
 def place_computer_ships(grid, ship_length):
     """Computer can place a random ship in the grid"""
-    row = random.randint(0, len(grid) - 1)
-    col = random.randint(0, len(grid[0]) - 1)
-    orientation = random.choice(['H', 'V'])
-    place_ship(grid, row, col, orientation, ship_length)
+    while True:
+        row = random.randint(0, len(grid) - 1)
+        col = random.randint(0, len(grid[0]) - 1)
+        orientation = random.choice(['H', 'V'])
+
+        if is_valid_placement(grid, row, col, orientation, ship_length):
+            place_ship(grid, row, col, orientation, ship_length)
+            break
 
 
 def make_guess(grid, row, col):
     """Make a guess at the grid"""
+    row = row - 1
+    col = col - 1
     if grid[row][col] == 'S':
         print("It's a hit!")
         grid[row][col] = 'H'
@@ -124,7 +175,6 @@ def main_game_loop(player_grid, computer_grid, ship_lengths):
 # Get grid size from user
 grid_size = get_grid_size()
 
-
 # Initialize a 5x5 grid for player and computer
 player_grid = initialize_grid(grid_size)
 computer_grid = initialize_grid(grid_size)
@@ -132,9 +182,8 @@ computer_grid = initialize_grid(grid_size)
 # Place multiple ships for player and computer
 ship_lengths = [5, 4, 3, 3, 2]
 
-place_multiple_ships(player_grid, ship_lengths,"player")
-place_multiple_ships(computer_grid, ship_lengths,"bot")
-
+place_multiple_ships(player_grid, ship_lengths, "player")
+place_multiple_ships(computer_grid, ship_lengths, "bot")
 
 # Start the main game loop
 main_game_loop(player_grid, computer_grid, ship_lengths)
